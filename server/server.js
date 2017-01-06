324ef3af06410053e0661208fb462b00a78e0102
Harshil Chokshi <Harsh@Harshils-MacBook-Pro.local>
2017-01-05 22:20:51 -0500

    Added updating notes feature

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ObjectID = require('mongodb').ObjectID;
+const _ = require('lodash');
 
 const mongoose = require('./../database/mongoose.js').mongoose;
 const Todo = require('./../database/models/todo.js').Todo;
@@ -40,7 +41,7 @@ app.get('/todos/:id', (req, res) => {
   if(!ObjectID.isValid(id))
   {
     res.status(404).send({
-      errorMessage : 'The id you passed is not a valid id'
+      errorMessage : 'The entered id is not valid'
     });
   }
   else
@@ -69,7 +70,7 @@ app.delete('/todos/:id', (req, res) => {
   if(!ObjectID.isValid(id))
   {
     res.status(404).send({
-      errorMessage : 'The id you returned is not a valid id'
+      errorMessage : 'The entered id is not valid'
     });
   }
   else
@@ -89,6 +90,49 @@ app.delete('/todos/:id', (req, res) => {
     }, (err) => {
       res.status(400).send({
         errorMessage : `An error occured while trying to delete a todo item: ${err}`
+      });
+    });
+  }
+});
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if(!ObjectID.isValid(id))
+  {
+    res.status(404).send({
+      errorMessage : 'The entered id is not valid'
+    });
+  }
+  else
+  {
+    if(_.isBoolean(body.completed) && body.completed)
+    {
+      body.completedAt = new Date().getTime();
+    }
+    else
+    {
+      body.completed = false;
+      body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set : body}, {new : true}).then((doc) => {
+      if(!doc)
+      {
+        res.status(404).send({
+          errorMessage : 'The entered id does not exist in the database'
+        });
+
+        return;
+      }
+
+      res.send({
+        todo : doc
+      });
+    }, (err) => {
+      res.status(400).send({
+        errorMessage: `Error occured while trying to uodate resource: ${err}`
       });
     });
   }
